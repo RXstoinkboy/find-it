4180ba6552ee8973062d4fd71b9ec639696433b1
RXstoinkboy <erykludwin@gmail.com>
2019-04-29 01:03:27 +0200

    working on slider

--- src/components/BackgroundImage.js
+++ src/components/BackgroundImage.js
@@ -1,35 +1,61 @@
-import React from 'react'
+import React, {Component} from 'react'
 import styles from './styles/BackgroundImage.module.scss'
 import {connect} from 'react-redux'
 
 import PropTypes  from 'prop-types'
 
-const BackgroundImage = props => {
-    let content;
+export class BackgroundImage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
 
-    if(props.collectedData.length >= 0){
-        content = props.collectedData.map((item, idx) => (
-            <div key={idx} className={styles.slide}>
-                <img className={idx === 0 ? [styles.bgImage, styles.bgImageActive].join(' ') : styles.bgImage} src={item.image} alt={item.name} />
-                <a className={idx === 0 ? [styles.link, styles.linkActive].join(' ') : styles.link} href={item.url}>{item.name}</a>
-            </div>
-        ))
+        }
     }
 
-    return (
-        <div className={styles.wrapper}>
-            <button className={[styles.button, styles.buttonLeft].join(' ')}></button>
-            <button className={[styles.button, styles.buttonRight].join(' ')}></button>
-            
-            {content}
-            
-            <div className="dotWrapper">
-                {content.map((item, idx) => (
-                    <div className="dot" key={idx}></div>        
-                ))}
+    handleLeft = () => {
+        console.log('left')
+    }
+
+    handleRight = e => {
+        // const currentActive = (content.find(item => item.props.children[0].props.className == [styles.bgImage, styles.bgImageActive].join(' ')).key)
+        // // console.log(content[0].props.children[0].props.className)
+
+        // // change bg image to next in array
+        // content[currentActive].props.children[0].props.className = styles.bgImage;
+        // content[currentActive + 1].props.children[0].props.className = [styles.bgImage, styles.bgImageActive].join(' ');
+        // console.log(currentActive)
+    }
+    
+    render(){
+        // render images and corresponding link to Yelp
+        // add active classes to first in the row
+        let content;
+
+        if(this.props.collectedData.length >= 0){
+            content = this.props.collectedData.map((item, idx) => (
+                <div key={idx} className={styles.slide}>
+                    <img className={idx === 0 ? [styles.bgImage, styles.bgImageActive].join(' ') : styles.bgImage} src={item.image} alt={item.name} />
+                    <a className={idx === 0 ? [styles.link, styles.linkActive].join(' ') : styles.link} href={item.url}>{item.name}</a>
+                </div>
+            ))
+        }
+
+        return (
+            <div className={styles.wrapper}>
+                <button onClick={this.handleLeft} className={[styles.button, styles.buttonLeft].join(' ')}></button>
+                <button onClick={this.handleRight} className={[styles.button, styles.buttonRight].join(' ')}></button>
+                
+                {content}
+                
+                {/* <div className={styles.dotsWrapper}>
+                    {content.map((item, idx) => (
+                        <div className={styles.dot} key={idx}></div>        
+                    ))}
+                </div> */}
             </div>
-        </div>
-    );
+        );
+
+    }
 };
 
 const mapStateToProps = state => {
