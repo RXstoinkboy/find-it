5f840b6bd882369d381d681d95883ea18cb996ac
RXstoinkboy <erykludwin@gmail.com>
2019-05-26 23:59:32 +0200

    styling of results page

--- src/components/Results/Result.js
+++ src/components/Results/Result.js
@@ -5,25 +5,32 @@ const Result = () => {
     return (
         <div className={styles.result}>
             <div className={styles.leftSide}></div>
-            <div className={styles.rightSide}>
+            <section className={styles.rightSide}>
                 <h3 className={styles.restaurantName}>1. Restaurant name</h3>
-                <div className={styles.rate}>
+                <section className={styles.rate}>
                     <div className={styles.stars}>
-                        <div className={styles.star}>*****</div>
+                        <div className={styles.star} />
+                        <div className={styles.star} />
+                        <div className={styles.star} />
+                        <div className={styles.star} />
+                        <div className={styles.star} />
                     </div>
-                    <p className={styles.reviews}>100 reviews</p>
-                </div>
-                <div className={styles.infoRow}>
+                    <p className={styles.reviews}><span className={styles.reviewsNum}>100</span> reviews</p>
+                </section>
+                <section className={styles.infoRow}>
                     <p className={styles.price}>$$</p>
                     <div className={styles.categories}>
                         <a href="#" className={styles.category}>Italian</a>
                         <a href="#" className={styles.category}>Italian</a>
                         <a href="#" className={styles.category}>Italian</a>
                     </div>
-                </div>
+                </section>
                 <a href="tel:+12600500400">{"12 600 500 400"}</a>
-                <p className={styles.address}></p>
-            </div>
+                <section className={styles.address}>
+                    <p>Asnyka 111</p>
+                    <p>32-020 Wieliczka</p>
+                </section>
+            </section>
         </div>
     );
 };
